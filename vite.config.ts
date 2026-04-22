cbe0dcf72465fb7258aecf9d00bb770c868f14b8
ManepalliBalasaiRajaHasini <hasinimanepalli518@gmail.com>
2026-04-22 21:53:50 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/storylearn-ai/"
+    build: {
+      outDir: "dist"
+    }
   }
 });
